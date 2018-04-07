8bd49b91744b28a83ee84c19f2f3d472afe1f666
Brienna Herold <brienna.herold@gmail.com>
2018-04-07 18:16:03 -0400

    Load 'about' and 'degrees' data

--- Project 2/script.js
+++ Project 2/script.js
@@ -23,7 +23,7 @@ $(document).ready(function() {
             data: path,
         }).fail(function(err) {
             console.log('Error: ' + err);
-        });                     // Note: No done here
+        });                     // Note: No .done() here
     }
 
     /**************************** SCROLLMAGIC ****************************/
@@ -43,12 +43,132 @@ $(document).ready(function() {
                 triggerElement: panels[i]
             })
             .setPin(panels[i])
-            .addIndicators() // Debug: Add indicators (Requires additional plugin)
+            .addIndicators() // debugging (requires additional plugin)
             .addTo(controller);
     }
 
-    /**************************** NEWS ****************************/
-    // Get news
+    /**************************** ABOUT ****************************/
+    // Query "About" data
+    xhr('get', { path : '/about/' }).done(function(json) {
+        processAbout(json);
+    })
+
+    /**
+     * Processes and renders result of AJAX call to get 'About' information.
+     *
+     * @param data the returned result.
+     **/
+    function processAbout(data) {
+        console.log("Processing 'About'...");
+
+        // Get "About" data 
+        var title = data.title;
+        var desc = data.description;
+        var quote = data.quote;
+        var quoteAuthor = data.quoteAuthor;
+
+        // Add the pieces to "About" panel
+        $('#about .inner').children().first().text(title)
+            .next().text(desc)
+            .next().text(quote)
+            .next().text("- " + quoteAuthor);
+    }
+
+    /**************************** DEGREES ****************************/
+    // Query "Degrees" data
+    xhr('get', { path : '/degrees/' }).done(function(json) {
+        processDegrees(json);
+    });
+
+    /**
+     * Processes and renders result of AJAX call to get degrees.
+     *
+     * @param data object representing either "graduate" or "undergraduate" degrees.
+     **/
+    function processDegrees(data) {
+        // Process undergraduate & graduate degrees separately
+        var category;
+        for (var level in data) {
+            category = data[level];
+            if (level == "graduate") {
+                certifications = category[3];
+                category = category.slice(0, 3);                
+            }
+            
+            // Get info for each degree
+            console.log("Processing " + level + " degrees...");
+            $.each(category, function(i, degree) {
+                // Add degree title & description to Degree panel
+                $('#' + level).append('<div class="degrees" data-level="' + level + '" data-name="' 
+                    + degree.degreeName + '"><h3>' + degree.title + '</h3><p>' + degree.description);
+            });
+        }
+
+        // Add graduate certifications
+        $('#graduate').parent().append('<p>' + certifications.degreeName);
+
+        // Allow user to click on each degree to view more details in overlay
+        $('.degrees').each(function(index) {
+            $(this).on('click', function() {
+                // Reset overlay, leaving only its close button
+                $('#popup').children().slice(1).remove();
+
+                // Get degree details & add to overlay
+                var level = $(this).data('level');
+                var name = $(this).data('name');
+                for (var i = 0; i < data[level].length; i++) {
+                    var degree = data[level][i];
+                    if (degree.degreeName == name) {
+                        $('#popup').append('<h1>' + degree.title + '</h1><p>' + degree.description + '</p>');
+                        for (var j = 0; j < degree.concentrations.length; j++) {
+                            $('#popup').append('<li>' + degree.concentrations[j]);
+                        }
+
+                        // Add courses available for the degree
+                        var modifiedName = name.toUpperCase();
+                        if (level == 'graduate') {
+                            // Exception: IST is queried as IT so need to change its name here 
+                            if (modifiedName == 'IST') {
+                                modifiedName = 'IT';
+                            }
+                            modifiedName = 'MS' + modifiedName;
+                        } else if (level == 'undergraduate') {
+                            modifiedName = 'BS' + modifiedName;
+                        } 
+                        var coursePath = '/courses/degreeName=' + modifiedName;
+                        $('#popup').append('<h2>Courses</h2>');
+                        // Query courses with appropriately built path
+                        xhr('get', { path : coursePath }).done(function(json) {
+                            $.each(json.courses, function(i, course) {
+                                // Add each course
+                                $('#popup').append('<li>' + course);
+                            })
+                        });
+                    }
+                }
+
+                // Show overlay
+                $('#popup').popup('show');
+            });
+        });
+    }
+
+    // Toggle "Undergraduate" and "Graduate" views
+    $('#showGrad').on('click', function() {
+        $('#undergraduate').hide();
+        $('#graduate').parent().show();
+        $('#showUndergrad').css('color', 'gray');
+        $(this).css('color', 'white');
+    });
+    $('#showUndergrad').on('click', function() {
+        $('#graduate').parent().hide();
+        $('#undergraduate').show();
+        $('#showGrad').css('color', 'gray');
+        $(this).css('color', 'white');
+    });
+
+    /**************************** NEWS ****************************/ 
+    // Query "News" data 
     xhr('get', { path : '/news/' }).done(function(json) {
         processNews(json);
     });
@@ -59,10 +179,10 @@ $(document).ready(function() {
      * to the main panel, and setting up overlay to show the full versions, 
      * as well as a composite view of all news articles.
      * 
-     * @param data the returned json
+     * @param data the returned result.
      **/
     function processNews(data) {
-        console.log("Processing news...");
+        console.log("Processing 'News'...");
 
         var numShowing = 0;  // news count tracker
         var maxNumShowing = 2;  // max num of news stories to show on home page
@@ -122,10 +242,7 @@ $(document).ready(function() {
                 // Get data based on path specified in anchor tag's data attribute
                 // TO BE CHANGED: Probably don't need to make another query, can filter data by date
                 xhr('get', { path : $(this).data('ajaxpath') }).done(function(json) {
-                    var date = json.date;
-                    var title = json.title;
-                    var desc = json.description;
-                    $('#popup').append('<h1>' + title + '</h1><p>' + date + '<br><br>' + desc + '...</p>');
+                    $('#popup').append('<h1>' + json.title + '</h1><p>' + json.date + '<br><br>' + json.description + '...</p>');
                     // Also append "More news >>>" to overlay
                     $('#popup').append(moreNewsHtml).children().last().on('click', function() {
                         showAllNews();
